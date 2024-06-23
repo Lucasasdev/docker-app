4d13816268b234d362b277ef2788930733f31ca3
Lucas Alves <Lucasasdev@gmail.com>
2024-06-23 12:19:14 -0300

    fix: reponse sentence

--- src/app.ts
+++ src/app.ts
@@ -4,7 +4,7 @@ const app = express();
 const port = 3000;
 
 app.get("/", (req: Request, res: Response) => {
-  res.send("First project using Docker!").status(200);
+  res.send("First project running on Docker Container!").status(200);
 });
 
 app.listen(port, () => {
