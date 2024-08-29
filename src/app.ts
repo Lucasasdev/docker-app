f51c8f9b69b0878385e6728805eb6dde890e2d88
Lucas Alves <lucasasdev@gmail.com>
2024-08-29 08:23:36 -0300

    fix: remove unused import

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,5 @@
 import express, { Request, Response } from "express";
 import userRouter from "./routes";
-import * as repository from "./repositories/repository";
 const app = express();
 
 app.use(express.json());
